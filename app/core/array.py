8aa3d21931a48f88a1e098b47691e02b74c8e5bd
Joshua Wise <joshua.wise722@gmail.com>
2026-05-31 19:40:29 -0400

    WIP StaticArray class

--- app/core/array.py
+++ app/core/array.py
@@ -2,7 +2,7 @@ from app.core.base import BaseDataStructure
 from app.core.mixins import BoundsCheckMixin
 
 
-class Array[T](BoundsCheckMixin, BaseDataStructure):
+class StaticArray[T](BoundsCheckMixin, BaseDataStructure):
     def __init__(self, size: int):
         super().__init__(size)
 
@@ -12,7 +12,6 @@ class Array[T](BoundsCheckMixin, BaseDataStructure):
         Arguments:
             index: The index place to be inserted.
             value: Generic typic of the value to be inserted.
-
         """
         self._bounds_check(index)
 
@@ -30,27 +29,56 @@ class Array[T](BoundsCheckMixin, BaseDataStructure):
 
         Returns:
             boolean of true or false depending on deletion
-
         """
-        return False
+        if self._bounds_check(index):
+            self._data[index] = None
+        return True
 
-    def contains(self, value: T | None, index: int | None) -> bool:
+    def contains(self, value: T) -> bool:
         """Check if a value is contained inside of the array.
 
         Arguments:
             value: Generic typing of the value, optionally passed
-            index: Index location of the item to check if it exists
         """
+        if value in self._data:
+            return True
+
         return False
 
     def traversal(self, value: T | None, index: int | None) -> bool:
         """Traversal of the array.
 
+        Defaults to traversing the entire array is no index is passed.
+        Also allows traversal until a value is found.
+
         Arguments:
             value: The value to be traversed to.
             index: The index location of desired traversal.
         """
-        return False
+        if not self.size:
+            return False
+
+        if index is not None:
+            self._bounds_check(index)
+            for i in range(index + 1):
+                current_item = self._data[i]
+                # TODO Need to add some sort of item for now
+            return True
+
+        if value is not None:
+            for i in range(self.size):
+                current_item = self._data[i]
+                # TODO Some function here
+
+                if current_item == value:
+                    return True
+            return False
+
+        for i in range(self.size):
+            current_item = self._data[i]
+            # TODO Need some function here
+
+        return True
 
     def display(self, value: T, index: int | None) -> list[T | None]:
         """Display the array until the index given or value.
