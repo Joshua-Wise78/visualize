6dac05eb0ab63cb0c884470348748d1fd53d5f83
Joshua Wise <joshua.wise722@gmail.com>
2026-06-14 21:07:39 -0400

    WIP delete value

--- app/core/doubly_linked_list.py
+++ app/core/doubly_linked_list.py
@@ -25,29 +25,8 @@ class DoublyLinkedList[T]:
 
         self.size = 0
 
-    def insert(self, value: T, index: int | None = None) -> None:
-        """Insert a value inside of the doubly linked list.
-
-        Arguments:
-            value: The value to be inserted
-            index: Optional value to insert mid list instead of appending.
-        """
-        pass
-
-    def delete(self, value: T, index: int | None = None) -> None:
-        """Delete a value inside of the doubly linked list."""
-        pass
-
-    def get_node(self, value: T, index: int | None = None):
-        """Get a node from the doubly linked list."""
-        pass
-
-    def traversal(self, index: int | None = None):
-        """Traverse the doubly linked list."""
-        pass
-
     def _insert_between(
-        self, value: T, predecessor: Node[T], successor: Node[T]
+        self, value: T, predecessor: Node[T | None], successor: Node[T | None]
     ) -> None:
         """Univeral Helper method that will handle pointer logic.
 
@@ -56,7 +35,69 @@ class DoublyLinkedList[T]:
             predecessor: The previous node.
             successor: The next node in line.
         """
-        node = Node(value, prev=predecessor, next=successor)
+        node = Node[T | None](value)
+
+        node.prev = predecessor
+        node.next = successor
+
         predecessor.next = node
         successor.prev = node
-        self.size = self.size + 1
+
+        self.size += 1
+
+    def insert(self, value: T, index: int | None = None) -> None:
+        """Insert a value inside of the doubly linked list.
+
+        Arguments:
+            value: The value to be inserted
+            index: Optional value to insert mid list instead of appending.
+        """
+        if index is None or index >= self.size:
+            assert self.tail.prev is not None
+            self._insert_between(
+                value, predecessor=self.tail.prev, successor=self.tail
+            )
+            return
+
+        if index < 0:
+            index = 0
+
+        current: Node[T | None] | None = self.header.next
+
+        for _ in range(index):
+            if current is not None:
+                current = current.next
+
+        assert current is not None
+        assert current.prev is not None
+
+        self._insert_between(value, predecessor=current.prev, successor=current)
+
+    def delete(self, value: T, index: int | None = None) -> None:
+        """Delete a value inside of the doubly linked list.
+
+        Arguments:
+            value: The value to be deleted
+            index: Optional int value of the node location
+
+        """
+        if index is None or index >= self.size:
+            assert self.tail.prev is not None
+            return
+
+        if index < 0:
+            index = 0
+
+        current: Node[T | None] | None = self.header.next
+
+        for _ in range(index):
+            if current is not None:
+                current = current.next
+
+    def get_node(self, value: T, index: int | None = None):
+        """Get a node from the doubly linked list."""
+        pass
+
+    def traversal(self, index: int | None = None):
+        """Traverse the doubly linked list."""
+        pass
